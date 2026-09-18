69186c721988d56014bda136f02cca814c2e4c3b
Sanchit Jadhav <snaxchit@gmail.com>
2026-09-18 15:37:53 +0530

    Configure release build with signingConfig for direct shareable APK generation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("debug")
             optimization {
                 enable = false
             }
